359df999f880a9d4f57eb7fd0aee436fa89de56e
Kevin Moore <kevsm5041@gmail.com>
2022-08-06 12:21:44 -0600

    day 3

--- api/tags.js
+++ api/tags.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tagsRouter = express.Router();
-const { getAllTags } = require('../db');
+const { getAllTags, getPostsByTagName } = require('../db');
+const { requireUser } = require('./utils');
 
 tagsRouter.use((req, res, next) => {
   console.log("A request is being made to /tags");
@@ -14,6 +15,20 @@ tagsRouter.get('/', async (req, res) => {
     res.send({
         tags
     });
+});
+
+tagsRouter.get('/:tagName/posts', async (req, res, next) => {
+  // read the tagname from the params
+  const { tagname } = req.params;
+  try {
+    // use our method to get posts by tag name from the db
+    const postsWithTag = await getPostsByTagName(tagname);
+    // send out an object to the client { posts: // the posts }
+    res.send({ posts: postsWithTag })
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message })
+  }
 });
 
 module.exports = tagsRouter;
